7bcad839aab6a06acf0a28c271a921d74c1c2581
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-06-07 16:45:53 -0700

    Initial Setup & CI (#7)

---
+++ NAMS/Schedule/EventContext.swift
@@ -0,0 +1,25 @@
+//
+// This source file is part of the Neurodevelopment Assessment and Monitoring System (NAMS) project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import SpeziScheduler
+
+
+struct EventContext: Comparable, Identifiable {
+    let event: Event
+    let task: Task<NAMSTaskContext>
+    
+    
+    var id: Event.ID {
+        event.id
+    }
+    
+    
+    static func < (lhs: EventContext, rhs: EventContext) -> Bool {
+        lhs.event.scheduledAt < rhs.event.scheduledAt
+    }
+}
